94466b0650b32abab2567c8fceea04ef474f068d
ke1evra <dolgov.konstantin@gmail.com>
2020-02-03 22:39:59 +0300

    Добавил запрос за 1 день

--- mangoCallsDbWriter.js
+++ mangoCallsDbWriter.js
@@ -4,35 +4,50 @@ const colors = require('colors');
 
 moment.locale('ru');
 
-const date = '2018-01-01';
+const date = '2018-01-02';
 
 // Получает данные по звонкам в интервале
 const getCallsInRange = (dateFrom, dateTo) => {
     const url = `http://185.176.25.157:3000/mango/table?date_from=${moment(dateFrom).unix()}&date_to=${moment(dateTo).unix()}`;
-    console.log(`Получаем данные за период c ${moment(dateFrom).format('LLLL')} по ${moment(dateTo).format('LLLL')}`.cyan);
-    console.log('Выполняем запрос по адресу:'.cyan);
+    console.log(`Получаем данные за период c ${moment(dateFrom).format('LLL')} по ${moment(dateTo).format('LLL')}`.red);
+    console.log('Выполняем запрос по адресу:'.red);
     console.log(url);
 
-    axios.get(url)
+    return axios.get(url)
         .then(function(response) {
             console.log('Данные успешно получены:'.green);
             console.log(response.data.length);
+            return response.data;
         })
         .catch(function (err){
             console.error(err);
+            return err;
         })
         .finally(function () {
-            // console.log(`Скрипт выполнен`.cyan)
+            // console.log(`Скрипт выполнен`.red)
         });
 };
 
 
 // Получает данные за месяц с начала месяца от указанной даты
 const getCallsPerMonth = (date) => {
+    console.log(`Получаем данные за месяц на основе этой даты ${moment(date).format('ll')}`.red);
     const dateFrom = moment(date).startOf('month');
     const dateTo = moment(date).startOf('month').endOf('month');
-    getCallsInRange(dateFrom, dateTo);
+    getCallsInRange(dateFrom, dateTo).then((response) => {
+        console.table(response[0]);
+    });
 };
 
-getCallsPerMonth(date);
+// Получает данные за день с начала дня от указанной даты
+const getCallsPerDay = (date) => {
+    console.log(`Получаем данные за день на основе этой даты ${moment(date).format('ll')}`.red);
+    const dateFrom = moment(date).startOf('day');
+    const dateTo = moment(date).startOf('day').endOf('day');
+    getCallsInRange(dateFrom, dateTo).then((response) => {
+        console.table(response[0]);
+    });
+};
+
+getCallsPerDay(date);
 
