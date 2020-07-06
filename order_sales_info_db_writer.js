9b7d80e53f932ea6ed9b9c22bc1bd55a69c176cd
ke1eva <dolgov.konstantin@gmail.com>
2020-07-06 15:07:43 +0300

    дебаг1

--- order_sales_info_db_writer.js
+++ order_sales_info_db_writer.js
@@ -31,7 +31,7 @@ const getOrderSalesData = (from, to) => {
     return new Promise((resolve, reject) => {
         eccrmDB.query(query(from, to), null, (data, error) => {
             if (data) {
-                console.log(`Полученные данные\n${data}`);
+                console.log(`Полученные данные\n${JSON.stringify(data)}`);
                 resolve(data);
             } if (error) {
                 reject(error);
